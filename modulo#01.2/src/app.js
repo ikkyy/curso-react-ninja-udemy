5fd74854c9d6d65ccf4ea82e1abd4066169e2d80
Ikkyy <msndoikky@hotmail.com>
2019-07-29 13:02:36 -0300

    M1#A08 - Atributos do HTML

--- modulo#01.2/src/app.js
+++ modulo#01.2/src/app.js
@@ -6,8 +6,10 @@ import Title from './title'
 const App = React.createClass({
     render: function () {
         return (
-            <div>
+            <div className="container">
                 <Title name="Reinaldo Borin" />
+                <label htmlFor="input">Input</label>
+                <input type="text" id="input" aria-hidden={true} />
             </div>
         )
     }
