9a240c0a503d4c9aa013f54f5c0e160be9339445
Ikkyy <msndoikky@hotmail.com>
2019-07-29 13:08:26 -0300

    M1#A09 - getDefaultProps

--- modulo#01.2/src/app.js
+++ modulo#01.2/src/app.js
@@ -7,9 +7,7 @@ const App = React.createClass({
     render: function () {
         return (
             <div className="container">
-                <Title name="Reinaldo Borin" />
-                <label htmlFor="input">Input</label>
-                <input type="text" id="input" aria-hidden={true} />
+                <Title name="Reinaldo" lastname="Borin"/>
             </div>
         )
     }
--- modulo#01.2/src/title.js
+++ modulo#01.2/src/title.js
@@ -3,10 +3,16 @@
 import React from 'react'
 
 const Title = React.createClass({
+    getDefaultProps: function(){
+        return {
+            name: "Desconhecido",
+            lastname: "Sem sobrenome"
+        }
+    },
     render: function(){
         return (
             <div>
-               <h1>Olá, {this.props.name}</h1>
+               <h1>Olá, {this.props.name + ' ' + this.props.lastname}!</h1>
             </div>
         )
     }
